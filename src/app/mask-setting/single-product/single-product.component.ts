30b8b0d8f17569c87fe1d6dcc54989de464e1a39
zhaoxiaoxia <540578223@qq.com>
2020-04-23 09:35:10 +0800

    xg

--- src/app/mask-setting/single-product/single-product.component.ts
+++ src/app/mask-setting/single-product/single-product.component.ts
@@ -8,10 +8,14 @@ import {GlobalService} from '../../core/global.service';
   templateUrl: './single-product.component.html',
 })
 export class SingleProductComponent implements OnInit {
+  productList:any = [];
+  errorCode:any = [];   //未录入的一维码
   code:string = '';
   count:string = '';
   isShowCount:boolean = false;
+  isShowErrorCode:boolean = false;
 
+  type:number=1;
   uid:string;
   rollback_url:string;
   /**菜单id */
@@ -24,7 +28,7 @@ export class SingleProductComponent implements OnInit {
     private cookieStore:CookieStoreService,
     private globalService:GlobalService) {
     this.uid = this.cookieStore.getCookie('uid');
-    this.getPurchaseList('1');
+    this.getProductList();
     window.scrollTo(0,0);
   }
 
@@ -51,56 +55,86 @@ export class SingleProductComponent implements OnInit {
   }
 
   isShowCountDiv(){
-    if(this.code.indexOf('96') == 0){
+    if(this.code.indexOf('690708216') == 0 || this.code.indexOf('690708217') == 0){
+      this.type = (this.code.indexOf('690708216') == 0) ? 3 : 2;
       this.isShowCount = true;
     }else{
+      this.type = 1;
       this.isShowCount = false;
-    }
-  }
-  /**
-   * 录入单品
-   */
-  addSingleProduct(){
-    if(this.count){
-
     }
   }
 
   /**
    * 获取列表
-   * @param number
    */
-  getPurchaseList(number:string) {
-    // let url = 'getProductList?pr_type='+this.type+'&page='+number+'&sid='+this.cookieStore.getCookie('sid');
-    // if(this.keyword.trim() != '') {
-    //   url += '&keyword='+this.keyword.trim();
-    // }
-    // this.globalService.httpRequest('get',url)
-    //   .subscribe((data)=>{
-    //     this.purchaseList = data;
-    //     if(this.purchaseList['status'] == 202){
-    //       this.cookieStore.removeAll(this.rollback_url);
-    //       this.router.navigate(['/auth/login']);
-    //     }
-    //     if (this.purchaseList) {
-    //       if (this.purchaseList['result']['purchaseList']['current_page'] == this.purchaseList['result']['purchaseList']['last_page']) {
-    //         this.next = true;
-    //       } else {
-    //         this.next = false;
-    //       }
-    //       if (this.purchaseList['result']['purchaseList']['current_page'] == 1) {
-    //         this.prev = true;
-    //       } else {
-    //         this.prev = false;
-    //       }
-    //       this.selects = [];
-    //       for (let entry of this.purchaseList['result']['purchaseList']['data']) {
-    //         this.selects[entry['pr_id']] = false;
-    //       }
-    //       this.check = false;
-    //     }
-    //   });
+  getProductList() {
+    let url = 'getFrozenoProductList?sid='+this.cookieStore.getCookie('sid');
+    this.globalService.httpRequest('get',url)
+      .subscribe((data)=>{
+        this.productList = data;
+        if(this.productList['status'] == 202){
+          this.cookieStore.removeAll(this.rollback_url);
+          this.router.navigate(['/auth/login']);
+        }
+      });
+  }
 
+  /**
+   * 录入单品
+   */
+  onSubmit(){
+    if(this.code.trim() == ''){
+      alert('请扫入编号！');
+      return false;
+    }
+    if(this.code.length > 13 && this.type != 1){
+      alert("精华或导入仪的单品入库时，请做到一码一数量，再入库。");
+      return false;
+    }
+    let category_id=0;
+    if(this.code.indexOf('690708210') == 0){   //单品面膜装
+      category_id = this.globalService.mm_category_id;
+      this.type = 1;
+    }else if(this.code.indexOf('690708216') == 0){  //单品导入仪
+      category_id = this.globalService.dry_category_id;
+    }else if(this.code.indexOf('690708217') == 0){   //单品精华
+      category_id = this.globalService.jh_category_id;
+    }else{
+      alert('不存在此单品，无法入库！');
+      return false;
+    }
+    this.globalService.httpRequest('post','addFrozenoProduct',{
+      'code' : this.code,
+      'count' : this.count,
+      'category_id' :category_id,
+      'state' : 0,
+      'type' : this.type,
+      'u_id' : this.cookieStore.getCookie('uid'),
+      'sid':this.cookieStore.getCookie('sid')
+    }).subscribe((data)=>{
+        if(data['status'] == 201){
+          alert(data['msg']);
+          return false;
+        }else if(data['status'] == 200){
+          this.productList = data;
+          this.errorCode = data['errorCode'];
+          if(this.errorCode.length > 0){
+            this.isShowErrorCode = true;
+          }
+        }else if(data['status'] == 202){
+          this.cookieStore.removeAll(this.rollback_url);
+          this.router.navigate(['/auth/login']);
+        }
+      }
+    );
+  }
+
+  /**
+   * 关闭未录入信息
+   */
+  close(){
+    this.isShowErrorCode = false;
+    this.errorCode = [];
   }
 
 }
