e66a276f7f47ccb702e884a87c8d27207bdff459
zhaoxiaoxia <540578223@qq.com>
2020-07-03 16:55:39 +0800

    xg

--- src/app/marketing/coupon/coupon.component.ts
+++ src/app/marketing/coupon/coupon.component.ts
@@ -5,7 +5,16 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './coupon.component.html'
 })
 export class CouponComponent implements OnInit {
-
+    isStatus: number = 0;
+    assetsInfo:any = [];
+    approve_user:any=[];
+    follower_user:any=[];
+    rollback_url : string = '';
+    /**菜单id */
+    menu_id:any;
+    /** 权限 */
+    permissions : Array<any> = [];
+    menuInfos : any = [];
     constructor() { }
 
     ngOnInit() {
